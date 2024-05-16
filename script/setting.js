f69710e0816845b877d683bfec59da949838908d
SIKU-KR <peter012677@naver.com>
2024-05-16 16:04:48 +0900

    0516 16:04

--- script/setting.js
+++ script/setting.js
@@ -1,3 +1,11 @@
+var cookieExp = 1;
+var egg_index = 0;
+var background_index = 0;
+var music_index = 0;
+var egg_Array = ["eggimg1.webp","eggimg2.webp","eggimg3.webp","eggimg4.webp","eggimg5.webp"];
+var background_Array=["background.png","background2.png","background3.png"];
+var music_Array=["music1.webp","music2.webp","music3.webp","music4.webp","music5.webp",];
+
 $(document).ready(function(){
     $(".left").click(function(){
         var img = $(this).prev().children();
@@ -7,6 +15,12 @@ $(document).ready(function(){
     $(".right").click(function(){
         var img = $(this).prev().prev().children();
         rightbutton(img);
+    });
+
+    $("#save-button").on("click", function(){
+        setCookie("egg", egg_Array[egg_index], cookieExp);
+        setCookie("background", background_Array[background_index], cookieExp);
+        logAllCookies();
     });
 });
 
@@ -61,9 +75,25 @@ function rightbutton(obj){
     else if(attrid=="music_img")music_index=index
 }
 
-var egg_index = 0;
-var background_index = 0;
-var music_index = 0;
-var egg_Array = ["eggimg1.webp","eggimg2.webp","eggimg3.webp","eggimg4.webp","eggimg5.webp"];
-var background_Array=["scene1.webp","scene2.webp","scene3.webp","scene4.webp","scene5.webp",];
-var music_Array=["music1.webp","music2.webp","music3.webp","music4.webp","music5.webp",];
+function setCookie(name, value, days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    console.log()
+}
+
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
