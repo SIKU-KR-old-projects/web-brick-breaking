64e9ce4d67afe0ae01fbebd7332d593c5a181075
SIKU-KR <peter012677@naver.com>
2024-05-20 19:34:52 +0900

    1934

--- script/setting.js
+++ script/setting.js
@@ -1,12 +1,11 @@
-var cookieExp = 1;
-var egg_index = 0;
-var background_index = 0;
-var music_index = 0;
-var egg_Array = ["egg1.png", "egg2.png", "egg3.png"];
+var egg;//알 이미지
+var background;//배경
+var music;//음악
+var score;//점수
 var background_Array = ["background.png", "background2.png", "background3.png"];
-var music_Array = ["music1.webp", "music2.webp", "music3.webp", "music4.webp", "music5.webp"];
+var background_index = 0;
 
-$(document).ready(function () {
+$(document).ready(function(){
     var backgroundCookie = getCookie("background");
     if (backgroundCookie) {
         background_index = background_Array.indexOf(backgroundCookie);
@@ -14,77 +13,113 @@ $(document).ready(function () {
     }
     $('#background').css('background-image', 'url(../img/'+background_Array[background_index]+')');
 
-    $("#egg_img").attr("src", "./img/" + egg_Array[egg_index]);
-    $("#background_img").attr("src", "./img/" + background_Array[background_index]);
+    read_setting();
 
-    $(".left").click(function () {
-        var img = $(this).prev().children();
-        leftbutton(img);
-    }).on("mouseover", function(){
-        $(this).css("cursor", "pointer");
+    $(".setting_element").each(function(){
+        var container = $(this);
+        var img = container.find("img");
+        var explain = container.find(".explain");
+        var score_p = explain.find("p");
+        var require_score = score_p.attr("class");
+        console.log(require_score);
+
+
+        if(score>=Number(require_score)){
+            img.removeClass("hidden");
+            score_p.text("사용 가능");
+        }else{
+            score_p.text(require_score+"점 이상");
+        }
     });
 
-    $(".right").click(function () {
-        var img = $(this).prev().prev().children();
-        rightbutton(img);
-    }).on("mouseover", function(){
-        $(this).css("cursor", "pointer");
+    $(".setting").each(function(){
+        var setting = $(this);
+        var left = setting.find(".left");
+        var right = setting.find(".right");
+        var index=0;
+
+        left.click(function(){
+            var imgs = setting.find("img");
+            var current_img = imgs.eq(index);
+            var explain = setting.find(".explain");
+            var current_text = explain.eq(index);
+
+            index-=1;
+            index = index<0 ? imgs.length-1 : index; 
+            
+            var prev_img = imgs.eq(index);
+            current_img.addClass("alt");
+            prev_img.removeClass("alt");
+            prev_img.addClass("selected");
+            current_img.removeClass("selected")
+
+            var prev_text = explain.eq(index);
+            current_text.addClass("alt");
+            prev_text.removeClass("alt");
+
+        });
+        
+        right.click(function(){
+            var imgs = setting.find("img");
+            var current_img = imgs.eq(index);
+            var explain = setting.find(".explain");
+            var current_text = explain.eq(index);
+
+            index+=1;
+            index = index>imgs.length-1 ? 0 : index; 
+            
+            var prev_img = imgs.eq(index);
+            current_img.addClass("alt");
+            prev_img.removeClass("alt");
+            prev_img.addClass("selected");
+            current_img.removeClass("selected")
+
+            var prev_text = explain.eq(index);
+            current_text.addClass("alt");
+            prev_text.removeClass("alt");
+        });
     });
 
-    $("#save-button").on("click", function () {
-        setCookie("egg", egg_Array[egg_index], cookieExp);
-        setCookie("background", background_Array[background_index], cookieExp);
-        window.location.href = "start.html";  
-    });
+    $("#save_button").click(save_setting);
 });
 
-function leftbutton(obj) {
-    var index;
-    var Array;
-    var attrid = obj.attr("id");
-
-    if (attrid == "egg_img") {
-        index = egg_index;
-        Array = egg_Array;
-    } else if (attrid == "background_img") {
-        index = background_index;
-        Array = background_Array;
-    } else if (attrid == "music_img") {
-        index = music_index;
-        Array = music_Array;
-    }
-
-    if (index == 0) index = Array.length;
-    index = (index - 1) % Array.length;
-    obj.attr("src", "./img/" + Array[index]);
-
-    if (attrid == "egg_img") egg_index = index;
-    else if (attrid == "background_img") background_index = index;
-    else if (attrid == "music_img") music_index = index;
+//환경 변수 읽기
+function read_setting(){
+    egg;
+    background = $("#background");
+    music = $("#music");
+    score=0;
 }
 
-function rightbutton(obj) {
-    var index;
-    var Array;
-    var attrid = obj.attr("id");
+//setting의 역순
+function save_setting(){
+    //조건문으로 점수 몇점 머시기 해서 넣기
+    var setting = $(".setting");
+    var isTrue = true;
 
-    if (attrid == "egg_img") {
-        index = egg_index;
-        Array = egg_Array;
-    } else if (attrid == "background_img") {
-        index = background_index;
-        Array = background_Array;
-    } else if (attrid == "music_img") {
-        index = music_index;
-        Array = music_Array;
+    console.log("테스트");
+
+    for(var i=0; i<setting.length; i++){
+        var require_score = Number(setting.eq(i).find("p").attr("class"));
+
+        if(require_score>score){
+            isTrue=false;
+            console.log("테스트");
+        }
     }
 
-    index = (index + 1) % Array.length;
-    obj.attr("src", "./img/" + Array[index]);
-
-    if (attrid == "egg_img") egg_index = index;
-    else if (attrid == "background_img") background_index = index;
-    else if (attrid == "music_img") music_index = index;
+    if(isTrue){
+        var hiddenImgSrcs = $("div .selected").map(function() {
+            return $(this).attr("src");
+        }).get();
+        setCookie("egg", hiddenImgSrcs[0], 1); // expires in 7 days
+        setCookie("background", hiddenImgSrcs[1], 1); // expires in 7 days
+        setCookie("music", hiddenImgSrcs[2], 1); // expires in 7 days
+        window.location.href = "start.html";
+        console.log(hiddenImgSrcs);
+    }else{
+        alert("점수가 %d점 넘어야합니다.");
+    }
 }
 
 function setCookie(name, value, days) {
