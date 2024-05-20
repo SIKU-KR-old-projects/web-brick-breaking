c4d4f0ecc59df0a7c2ee28731274cc1421159315
davidpro08 <davidmoon0817@gmail.com>
2024-05-20 18:52:37 +0900

    setting 대충 만들기

--- script/setting.js
+++ script/setting.js
@@ -1,69 +1,142 @@
+var egg;//알 이미지
+var background;//배경
+var music;//음악
+var score;//점수
+
 $(document).ready(function(){
-    $(".left").click(function(){
-        var img = $(this).prev().children();
-        leftbutton(img);
+    read_setting();
+    img_setting();
+
+    $(".setting_element").each(function(){
+        var container = $(this);
+        var img = container.find("img");
+        var explain = container.find(".explain");
+        var score_p = explain.find("p");
+        var require_score = score_p.attr("class");
+        console.log(require_score);
+
+
+        if(score>=Number(require_score)){
+            img.removeClass("hidden");
+            score_p.text("사용 가능");
+        }else{
+            score_p.text(require_score+"점 이상");
+        }
     });
 
-    $(".right").click(function(){
-        var img = $(this).prev().prev().children();
-        rightbutton(img);
+    $(".setting").each(function(){
+        var setting = $(this);
+        var left = setting.find(".left");
+        var right = setting.find(".right");
+        var index=0;
+
+        left.click(function(){
+            var imgs = setting.find("img");
+            var current_img = imgs.eq(index);
+            var explain = setting.find(".explain");
+            var current_text = explain.eq(index);
+
+            index-=1;
+            index = index<0 ? imgs.length-1 : index; 
+            
+            var prev_img = imgs.eq(index);
+            current_img.addClass("alt");
+            prev_img.removeClass("alt");
+
+            var prev_text = explain.eq(index);
+            current_text.addClass("alt");
+            prev_text.removeClass("alt");
+
+        });
+        
+        right.click(function(){
+            var imgs = setting.find("img");
+            var current_img = imgs.eq(index);
+            var explain = setting.find(".explain");
+            var current_text = explain.eq(index);
+
+            index+=1;
+            index = index>imgs.length-1 ? 0 : index; 
+            
+            var prev_img = imgs.eq(index);
+            current_img.addClass("alt");
+            prev_img.removeClass("alt");
+
+            var prev_text = explain.eq(index);
+            current_text.addClass("alt");
+            prev_text.removeClass("alt");
+        });
+    });
+
+    $("#save_button").click(function(){
+        var setting = $(".setting");
+        var isTrue = true;
+        for(var i=0; i<setting.length; i++){
+            var current_text = setting.eq(i).find("p");
+            var require_score = Number(current_text.attr("class"));
+            
+            console.log(require_score);
+            if(require_score>score){
+                isTrue=false;
+                console.log("아녀아녀유");
+            }
+        }
+    
+        if(isTrue){
+            //egg.attr("src",egg);
+            //배경 이미지를 불러와서 사진 넣기
+            background.css("#background-image", "url(" + "#background_img".setting + ")");
+            console.log(background.css("background-image"));
+            //음악 속성을 가져와서 이미지 속성 src와 잘 버무려주기
+            //music.attr("src","./img/"+music.attr("src")+".webp");
+        }else{
+            alert("점수가 %d점 넘어야합니다.");
+        }
     });
 });
 
-function leftbutton(obj){
-    var index;
-    var Array;
-    var attrid = obj.attr("id");
-
-    //함수로 바꿀 수 있을까?
-    if(attrid=="egg_img"){
-        index=egg_index;
-        Array=egg_Array;
-    }else if(attrid=="background_img"){
-        index=background_index;
-        Array=background_Array;
-    }else if(attrid=="music_img"){
-        index=music_index;
-        Array=music_Array;
-    }
-
-    if(index==0) index=Array.length;
-    index=(index-1)%Array.length;
-    obj.attr("src", "./img/"+Array[index]);
-
-    if(attrid=="egg_img")egg_index=index;
-    else if(attrid=="background_img")background_index=index;
-    else if(attrid=="music_img")music_index=index
+//환경 변수 읽기
+function read_setting(){
+    egg;
+    background = $("#background");
+    music = $("#music");
+    score=150;
 }
 
-function rightbutton(obj){
-    var index;
-    var Array;
-    var attrid = obj.attr("id");
+function img_setting(){
+    $("#egg_img").attr("src",egg);
+    //배경 이미지를 불러와서 사진 넣기
+    $("#background_img").attr("src", background.css("background-image").slice(4,background.length-1));
+    //음악 속성을 가져와서 이미지 속성 src와 잘 버무려주기
+    $("#music_img").attr("src","./img/"+music.attr("src")+".png");
+    $("#score p ").text("최고 점수 : "+score+"점");
 
-    //함수로 바꿀 수 있을까?
-    if(attrid=="egg_img"){
-        index=egg_index;
-        Array=egg_Array;
-    }else if(attrid=="background_img"){
-        index=background_index;
-        Array=background_Array;
-    }else if(attrid=="music_img"){
-        index=music_index;
-        Array=music_Array;
-    }
-
-    index=(index+1)%Array.length;
-    obj.attr("src", "./img/"+Array[index]);
-
-    if(attrid=="egg_img")egg_index=index;
-    else if(attrid=="background_img")background_index=index;
-    else if(attrid=="music_img")music_index=index
+    
 }
 
-var egg_index = 0;
-var background_index = 0;
-var music_index = 0;
-var egg_Array = ["eggimg1.webp","eggimg2.webp","eggimg3.webp","eggimg4.webp","eggimg5.webp"];
-var background_Array=["scene1.webp","scene2.webp","scene3.webp","scene4.webp","scene5.webp",];
-var music_Array=["music1.webp","music2.webp","music3.webp","music4.webp","music5.webp",];
+//setting의 역순
+function save_setting(){
+    //조건문으로 점수 몇점 머시기 해서 넣기
+    var setting = $(".setting");
+    var isTrue = true;
+
+    for(var i=0; i<setting.length; i++){
+        var require_score = Number(setting.eq(i).find("p").attr("class"));
+
+        if(require_score>score){
+            isTrue=false;
+            console.log("아녀아녀유");
+        }
+    }
+
+    if(isTrue){
+        //egg.attr("src",egg);
+        //배경 이미지를 불러와서 사진 넣기
+        background.css("#background-image", "url(" + "#background_img".setting + ")");
+        console.log(background.css("background-image"));
+        //음악 속성을 가져와서 이미지 속성 src와 잘 버무려주기
+        //music.attr("src","./img/"+music.attr("src")+".webp");
+    }else{
+        alert("점수가 %d점 넘어야합니다.");
+    }
+}
